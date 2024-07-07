2335ba257fe997143e970d7d3099e4037de0d9fc
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-07 12:42:18 +0530

    Largest value of the N numbers

---
+++ JavaBasics/largestOfNNumbers.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class largestOfNNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the input: ");
+        int N = sc.nextInt();
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= N; i++) {
+            int J = sc.nextInt();
+            maxValue = Math.max(J, maxValue);
+
+        }
+        System.out.println(maxValue);
+    }
+}
--- JavaBasics/switchCase.java
+++ JavaBasics/switchCase.java
@@ -9,6 +9,7 @@ public class switchCase {
         System.out.println("Enter a month number: ");
         byte month = sc.nextByte();
 
+        //Normal switch case
         switch (month) {
             case 1:
                 System.out.println("January");
@@ -60,6 +61,71 @@ public class switchCase {
                 break;
             default:
                 System.out.println("Please choose a valid month number!");
+        }
+
+        //Enhanced switch case
+        switch (month) {
+            case 1 -> {
+                System.out.println("January");
+                System.out.println("31 days");
+            }
+            case 2 -> {
+                System.out.println("February");
+                System.out.println("28/29 days");
+            }
+            case 3 -> {
+                System.out.println("March");
+                System.out.println("31 days");
+            }
+            case 4 -> {
+                System.out.println("April");
+                System.out.println("30 days");
+            }
+            case 5 -> {
+                System.out.println("May");
+                System.out.println("31 days");
+            }
+            case 6 -> {
+                System.out.println("June");
+                System.out.println("30 days");
+            }
+            case 7 -> {
+                System.out.println("July");
+                System.out.println("31 days");
+            }
+            case 8 -> {
+                System.out.println("August");
+                System.out.println("31 days");
+            }
+            case 9 -> {
+                System.out.println("September");
+                System.out.println("30 days");
+            }
+            case 10 -> {
+                System.out.println("October");
+                System.out.println("31 days");
+            }
+            case 11 -> {
+                System.out.println("November");
+                System.out.println("30 days");
+            }
+            case 12 -> {
+                System.out.println("December");
+                System.out.println("31 days");
+            }
+            default ->
+                System.out.println("Please choose a valid month number!");
+        }
+
+        System.out.print("Enter a char: ");
+        char ch = sc.next().charAt(0);
+
+        switch (ch) {
+            case 'a', 'e', 'i', 'o', 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
         }
         sc.close();
     }
