8383431db4de034a89ab7af36f17af6897b001bd
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-07 17:30:17 +0530

    Random number till 5 program

---
+++ JavaBasics/Main.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int total = 0;
+        for (int i = 0; i <= num; i++) {
+            total += i;
+        }
+        System.out.println(total);
+
+    }
+}
---
+++ JavaBasics/randomNumber.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+
+public class randomNumber {
+
+    public static void main(String[] args) {
+        do {
+            int randomNum = (int) (Math.random() * 10 + 1);
+            System.out.println(randomNum);
+            if (randomNum == 5) {
+                System.out.println("Ending loop");
+                break;
+            }
+        } while (true);
+    }
+}
