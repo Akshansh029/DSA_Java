f390dc5ee921eae38c3ea02ccd503185f0a57aca
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-09 14:43:39 +0530

    Replace char of a string

---
+++ JavaBasics/palindromeString.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+public class palindromeString {
+
+    public static void main(String[] args) {
+        String str = "malayalam";
+        String reversedStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+        if (str.toLowerCase().equals(reversedStr.toLowerCase())) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
---
+++ JavaBasics/replaceChar.java
@@ -0,0 +1,24 @@
+package JavaBasics;
+
+import java.util.Scanner;
+//Write a program to return a string from a given string s where all occurrences of the first char of the string except the first occurrence have been changed to '$'.
+
+public class replaceChar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.next();
+        char firstChar = s.charAt(0);
+        String newString = "" + firstChar;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == firstChar) {
+                newString += '$';
+            } else {
+                newString += s.charAt(i);
+            }
+        }
+        System.out.println(newString);
+        sc.close();
+    }
+}
