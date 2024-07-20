13838fac50e8647a845265f565540e0813001284
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-21 01:18:20 +0530

    practice

---
+++ JavaBasics/passByReference.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+
+public class passByReference {
+
+    public static void pass(int a) {
+        a++;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        pass(a);
+        System.out.println(a);
+    }
+}
