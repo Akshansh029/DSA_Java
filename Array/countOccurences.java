22ff9b3e18f693956f778bd7f4dad50e104e9e40
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-21 11:17:54 +0530

    count occurences in array

---
+++ Array/countOccurences.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class countOccurences {
+
+    static int countInt(int[] arr, int num) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 7, 2, 7, 6, 9, 3, 2, 7, 2, 1};
+        int count = countInt(arr1, 12);
+        System.out.printf("The number of times 12 occured: %d", count);
+    }
+}
---
+++ JavaBasics/asciiValue.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class asciiValue {
+
+    public static void main(String[] args) {
+        char a = 'A';
+        int ascii = a;
+        System.out.println(ascii);
+    }
+}
