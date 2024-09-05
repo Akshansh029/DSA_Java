848cef0d061aafa50c5bd4d7bdebaa22ee002c4e
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-09-05 09:46:10 +0530

    Added edge case check

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -5,9 +5,11 @@ public class BubbleSort {
 
     static int[] bubbleSort(int[] arr) {
         int N = arr.length;
+        boolean swapped;
 
         // Run the steps N - 1 times
         for (int i = 0; i < N - 1; i++) {
+            swapped = false; // Checking if swapping occured or not
 
             // For each step largest element will come at last index
             for (int j = 1; j < N - i; j++) {
@@ -17,7 +19,13 @@ public class BubbleSort {
                     int temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            }
+
+            // Break if the array is already sorted based on the boolean 
+            if (!swapped) {
+                break;
             }
         }
 
