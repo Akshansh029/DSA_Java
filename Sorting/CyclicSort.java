dd5059131091590c003c0065f5cf28dc9483b07e
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-09-11 10:11:55 +0530

    Corrected cyclic sort for a more general form

--- Sorting/CyclicSort.java
+++ Sorting/CyclicSort.java
@@ -9,7 +9,14 @@ public class CyclicSort {
         while (i < arr.length) {
             int correct = arr[i] - 1;
 
-            if (i != arr[i] - 1) {
+            // if (i != arr[i] - 1) {
+            //     int temp = arr[i];
+            //     arr[i] = arr[correct];
+            //     arr[correct] = temp;
+            // } else {
+            //     i++;
+            // }
+            if (arr[i] != arr[correct]) {
                 int temp = arr[i];
                 arr[i] = arr[correct];
                 arr[correct] = temp;
