04c3aba1c9a435ab1a58a13ba72f1822d88dc279
Akshansh <akshanshsingh00@gmail.com>
2024-11-01 11:21:16 +0530

    Custom Arraylist using Generics

---
+++ Oops/Generics/CustomGenArrayList.java
@@ -0,0 +1,73 @@
+
+import java.util.Arrays;
+
+public class CustomGenArrayList<T> {
+
+    private Object[] data;
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomGenArrayList() {
+        data = new Object[DEFAULT_SIZE];
+    }
+
+    public void add(T num) {
+        if (isFull()) {
+            resize();
+        }
+
+        data[size++] = num;
+    }
+
+    public T removeLast() {
+        T removed = (T) data[--size];
+        return removed;
+    }
+
+    public T get(int index) {
+        return (T) data[index];
+    }
+
+    public void set(int index, int value) {
+        data[index] = value;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomGenArrayList data=" + Arrays.toString(data) + ", size=" + size + '}';
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    private void resize() {
+        Object[] temp = new Object[2 * data.length];
+
+        // copy the current items in the new array
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+
+        data = temp;
+    }
+
+    public static void main(String[] args) {
+        CustomGenArrayList<Integer> list = new CustomGenArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(300);
+        list.add(400);
+        list.add(500);
+        System.out.println(list);
+
+        CustomGenArrayList<String> list2 = new CustomGenArrayList<>();
+        list2.add("A");
+        list2.add("B");
+        list2.add("C");
+        list2.add("D");
+        list2.add("E");
+
+        System.out.println(list2);
+    }
+}
