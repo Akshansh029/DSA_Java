a435332637a9e500669306f928b5a0af62489104
Akshansh <akshanshsingh00@gmail.com>
2024-11-02 10:24:00 +0530

    Lambda functions

--- Oops/Generics/CustomGenArrayList.java
+++ Oops/Generics/CustomGenArrayList.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class CustomGenArrayList<T> {
 
     private Object[] data;
-    private static int DEFAULT_SIZE = 10;
+    private static final int DEFAULT_SIZE = 10;
     private int size = 0;
 
     public CustomGenArrayList() {
