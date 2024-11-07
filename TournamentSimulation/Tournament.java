e1687f64814b23eaf86a2561b2ae20b5a55e2dfe
Akshansh <akshanshsingh00@gmail.com>
2024-11-07 10:35:33 +0530

    Started with tournament simulation program

---
+++ TournamentSimulation/Tournament.java
@@ -0,0 +1,28 @@
+
+public class Tournament {
+
+    Team[] teams;
+    Group[] groups;
+    Team[] knockouTeams;
+    String finalWinner;
+
+    public void createGroups() {
+
+    }
+
+    public void playGroupStages() {
+
+    }
+
+    public void determinePlayoffTeams() {
+
+    }
+
+    public void playKnockouts() {
+
+    }
+
+    public void determineTopScorer() {
+
+    }
+}
