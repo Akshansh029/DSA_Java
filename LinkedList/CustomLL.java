ffb8f61f58d70abc949a467afb570ffbebe0a22b
Akshansh <akshanshsingh00@gmail.com>
2024-12-13 11:41:34 +0530

    Traversal and insertion completed of LL

--- LinkedList/CustomLL.java
+++ LinkedList/CustomLL.java
@@ -9,6 +9,61 @@ public class CustomLL {
         this.size = 0;
     }
 
+    public void insertAtFirst(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void insertAtIndex(int value, int index) {
+
+        if (index == 0) {
+            insertAtFirst(value);
+            return;
+        }
+        if (index == size) {
+            insertAtIndex(value, index);
+            return;
+        }
+        Node curr = head;
+
+        for (int i = 1; i < index; i++) {
+            curr = curr.next;
+        }
+
+        Node newNode = new Node(value, curr.next);
+        curr.next = newNode;
+        size++;
+    }
+
+    public void insertAtLast(int value) {
+
+        if (tail == null) {
+            insertAtFirst(value);
+        } else {
+            Node newNode = new Node(value);
+            tail.next = newNode;
+            tail = newNode;
+            size += 1;
+        }
+    }
+
+    public void print() {
+        Node current = this.head;
+
+        while (current != null) {
+            System.out.print(current.value + " -> ");
+            current = current.next;
+        }
+
+        System.out.print("null");
+    }
+
     private class Node {
 
         private int value;
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -5,7 +5,24 @@ public class Main {
 
     public static void main(String[] args) {
         CustomLL list = new CustomLL();
+        list.insertAtLast(10);
+        list.insertAtLast(20);
+        list.insertAtLast(40);
+        list.insertAtLast(50);
+        list.insertAtIndex(30, 2);
+        list.print();
+
         LinkedList<Integer> list1 = new LinkedList<>();
-        System.out.println();
+
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+        // for (int i = 0; i < list1.size(); i++) {
+        //     System.out.print(list1.get(i) + " -> ");
+        // }
+        // System.out.print("null");
     }
 }
