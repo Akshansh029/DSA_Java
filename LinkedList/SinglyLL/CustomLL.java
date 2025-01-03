20e005a0868deb6dfafdbbeac61d6e5470889c72
Akshansh <akshanshsingh00@gmail.com>
2025-01-03 13:10:45 +0530

    If LL contains a cycle

--- LinkedList/SinglyLL/CustomLL.java
+++ LinkedList/SinglyLL/CustomLL.java
@@ -202,6 +202,25 @@ public class CustomLL {
         return ans;
     }
 
+    // Has cycle in LL
+    public static boolean hasCycle(Node head) {
+        if (head == null) {
+            return false;
+        }
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private class Node {
 
         private int value;
