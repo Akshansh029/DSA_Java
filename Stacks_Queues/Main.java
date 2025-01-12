7b5ac2c610951e9b0c65b55591a7800b897aa82f
Akshansh <akshanshsingh00@gmail.com>
2025-01-12 10:10:02 +0530

    Stacks and Queues started

--- Stacks_Queues/Main.java
+++ Stacks_Queues/Main.java
@@ -1,11 +1,13 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
-        // Queue que = 
+        Queue<Integer> que = new LinkedList<>();
         // Insertion
         stack.push(10);
         stack.push(20);
